18ad7b3be5cdb5ee4bb29ec8188664318ec924f4
Akram <akra200m@gmail.com>
2019-08-16 12:11:17 -0400

    DeployFinal

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from functools import wraps
 
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://***REMOVED***?sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://bfdfczbsfcinwo:0d5b3fa1df74d1919e4d6a0954bb4a570fa1d6c955f346991b7fd9f2fad430f5@ec2-75-101-133-29.compute-1.amazonaws.com:5432/ddp0q09isres7h?sslmode=require'
 #app.config['SQLALCHEMY_DATABASE_URI']='****'
-app.secret_key='***REMOVED***'
+app.secret_key='akram123'
 
 db=SQLAlchemy(app)
 
@@ -130,7 +130,7 @@ def quiz():
 
 
 if __name__== "__main__":
-    #app.secret_key='***REMOVED***'
+    #app.secret_key='akram123'
     app.config['SESSION_TYPE'] = 'filesystem'
     #app.jinja_env.auto_reload = True
     #app.config['TEMPLATES_AUTO_RELOAD'] = True
