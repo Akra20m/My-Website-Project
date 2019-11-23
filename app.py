bec8f9aa6ce7f5fe11bbc280da0b3c769bcd58b1
Akram <akra200m@gmail.com>
2019-11-23 14:36:19 -0500

    complete restructuring and separate blog part into its own project

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
-from passlib.hash import sha256_crypt
-from functools import wraps
+import os
 
 
 app=Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI']='postgres://202601?sslmode=require'
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:202601@localhost/project1'
-app.secret_key='akram123'
+#app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DB_HEROKU')
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('LOCALPW')
+app.secret_key=os.environ.get('SECRET_KEY')
 
 db=SQLAlchemy(app)
 
@@ -21,20 +19,6 @@ class Data(db.Model):
     def __init__(self,word,defination):
         self.word=word
         self.defination=defination
-
-class Users(db.Model):
-    __tablename__='registration data'
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.Text, unique=False)
-    email=db.Column(db.Text, unique=True)
-    username=db.Column(db.Text, unique=True)
-    password=db.Column(db.Text, unique=False)
-
-    def __init__(self,name,email,username,password):
-        self.name=name
-        self.email=email
-        self.username=username
-        self.password=password
 
 @app.route("/main", methods=['GET', 'POST'])
 def main():
@@ -53,66 +37,6 @@ def main():
 def index():
     return render_template("index.html")
 
-class RegisterForm(Form):
-    name=StringField('Name',[validators.length(min=1, max=50)])
-    username=StringField('Username',[validators.length(min=4, max=25)])
-    email=StringField('Email',[validators.length(min=6, max=50)])
-    password=PasswordField('Password',[validators.DataRequired(),
-    validators.EqualTo('confirm', message='Passwords do not match')])
-    confirm=PasswordField('Confirm Password')
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form=RegisterForm(request.form)
-    if request.method=='POST' and form.validate():
-        name= form.name.data
-        email=form.email.data
-        username=form.username.data
-        password=sha256_crypt.encrypt(str(form.password.data))
-        users=Users(name,email,username,password)
-        db.session.add(users)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('registration.html', form=form)
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    if request.method=='POST':
-        username=request.form["username"]
-        password=request.form["password"]
-        usernamecandidate=Users.query.filter_by(username=username).first()
-        if usernamecandidate != None:
-            if sha256_crypt.verify(password,usernamecandidate.password):
-                print(" everything matched")
-                session['logged']=True
-                session['username']=username
-                return redirect(url_for('panel'))
-            else:
-                print("****")
-                error='wrong password'
-                return render_template('login.html',error=error)
-        else:
-            error=' username not found'
-            print("NONE matched")
-            return render_template('login.html',error=error)
-    return render_template('login.html')
-
-def logged_in(s):
-    @wraps(s)
-    def wrap(*args, **kwargs):
-        if 'logged' in session:
-            return s(*args, **kwargs)
-        else:
-            return redirect(url_for('login'))
-    return wrap
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('login'))
-@app.route('/panel')
-@logged_in
-def panel():
-    return render_template('panel.html')
 
 @app.route('/quiz')
 def quiz():
