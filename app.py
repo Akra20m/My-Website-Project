2f689bf7c6b1bdd6ccc2e3de6a6f609a7c07d020
Akra20m <akra200m@gmail.com>
2020-03-09 14:47:34 -0400

    clean the code

--- app.py
+++ app.py
@@ -3,36 +3,40 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
-#app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('LOCALPW')
-app.secret_key=os.environ.get('SECRET_KEY')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('LOCALPW')
+app.secret_key = os.environ.get('SECRET_KEY')
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
+
 
 class Data(db.Model):
-    __tablename__='dictionary'
-    id=db.Column(db.Integer, primary_key=True)
-    word=db.Column(db.Text, unique=False)
-    defination=db.Column(db.Text, unique=False)
+    __tablename__ = 'dictionary'
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.Text, unique=False)
+    defination = db.Column(db.Text, unique=False)
 
-    def __init__(self,word,defination):
-        self.word=word
-        self.defination=defination
+    def __init__(self, word, defination):
+        self.word = word
+        self.defination = defination
+
 
 @app.route("/main", methods=['GET', 'POST'])
 def main():
-    if request.method=='POST':
-        words=request.form["word"].lower().capitalize()
+    if request.method == 'POST':
+        words = request.form["word"].lower().capitalize()
         print(words)
 
-        news=Data.query.filter_by(word=words).first()
+        news = Data.query.filter_by(word=words).first()
         print(news)
-        if news!=None:
-            return render_template("main.html", mynews=news.defination,mynews1=news.word)
-        return render_template("main.html",text="We don't have the meaning for this word")
-    if request.method=='GET':
+        if news != None:
+            return render_template("main.html", mynews=news.defination, mynews1=news.word)
+        return render_template("main.html", text="We don't have the meaning for this word")
+    if request.method == 'GET':
         return render_template("main.html")
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -43,7 +47,7 @@ def quiz():
     return render_template('quiz.html')
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.debug=True
+    app.debug = True
     app.run()
--- static/panel.js
+++ static/panel.js
@@ -1,88 +1,89 @@
-var questions = [{
-    "question":"Who invented the 'C' language?",
-    "A":"Dennis Ritchie",
-    "B":"James Gosling",
-    "C":"Guido van Rossum",
-    "D":"Brendan Eich",
-    "answer":"1"
-  },{
-    "question":"How many types of heading does an HTML contain?",
-    "A":"2",
-    "B":"4",
-    "C":"6",
-    "D":"10",
-    "answer":"3"
-  },{
-    "question":"What backend framework was used to build this website?",
-    "A":"Django",
-    "B":"Flask",
-    "C":"Express",
-    "D":"Ruby on Rails",
-    "answer":"2"
+var questions = [
+  {
+    question: "Who invented the 'C' language?",
+    A: "Dennis Ritchie",
+    B: "James Gosling",
+    C: "Guido van Rossum",
+    D: "Brendan Eich",
+    answer: "1"
+  },
+  {
+    question: "How many types of heading does an HTML contain?",
+    A: "2",
+    B: "4",
+    C: "6",
+    D: "10",
+    answer: "3"
+  },
+  {
+    question: "What backend framework was used to build this website?",
+    A: "Django",
+    B: "Flask",
+    C: "Express",
+    D: "Ruby on Rails",
+    answer: "2"
   }
 ];
 
-var scoreCount=0;
-var currentQuestion=0;
-var quizContainer= document.getElementById('quiz');
-var questionElement= document.getElementById('question');
-var optionA= document.getElementById('A');
-var optionB= document.getElementById('B');
-var optionC= document.getElementById('C');
-var optionD= document.getElementById('D');
-var next= document.getElementById('next');
-var score= document.getElementById('score');
-var tryAgain= document.getElementById('tryAgain');
+var scoreCount = 0;
+var currentQuestion = 0;
+var quizContainer = document.getElementById("quiz");
+var questionElement = document.getElementById("question");
+var optionA = document.getElementById("A");
+var optionB = document.getElementById("B");
+var optionC = document.getElementById("C");
+var optionD = document.getElementById("D");
+var next = document.getElementById("next");
+var score = document.getElementById("score");
+var tryAgain = document.getElementById("tryAgain");
 
-var totalQuestions=questions.length;
+var totalQuestions = questions.length;
 
-
-function loadQuestion (questionNum) {
-  questionElement.textContent=questions[questionNum].question;
-  optionA.textContent=questions[questionNum].A;
-  optionB.textContent=questions[questionNum].B;
-  optionC.textContent=questions[questionNum].C;
-  optionD.textContent=questions[questionNum].D;
+function loadQuestion(questionNum) {
+  questionElement.textContent = questions[questionNum].question;
+  optionA.textContent = questions[questionNum].A;
+  optionB.textContent = questions[questionNum].B;
+  optionC.textContent = questions[questionNum].C;
+  optionD.textContent = questions[questionNum].D;
 }
 
-function nextQuestion () {
-  var selected = document.querySelector('input[type=radio]:checked');
+function nextQuestion() {
+  var selected = document.querySelector("input[type=radio]:checked");
   var useranswer = selected.value;
-  if(useranswer == questions[currentQuestion].answer){
+  if (useranswer == questions[currentQuestion].answer) {
     scoreCount += 1;
   }
   deactivate();
-  selected.checked =false;
+  selected.checked = false;
   currentQuestion++;
-  if(currentQuestion==totalQuestions){
-    quizContainer.style.display='none';
-    next.style.display='none';
-    score.style.display='';
-    tryAgain.style.display='';
-    score.innerHTML='Your Score <br>' + scoreCount;
+  if (currentQuestion == totalQuestions) {
+    quizContainer.style.display = "none";
+    next.style.display = "none";
+    score.style.display = "";
+    tryAgain.style.display = "";
+    score.innerHTML = "Your Score <br>" + scoreCount;
     return;
-
   }
   loadQuestion(currentQuestion);
 }
 
-function activate(){
-    var resetBtn = document.getElementById('next');
-    resetBtn.disabled = false;
+function activate() {
+  var resetBtn = document.getElementById("next");
+  resetBtn.disabled = false;
 }
 
-function deactivate(){
-    var resetBtn = document.getElementById('next');
-    resetBtn.disabled = true;
+function deactivate() {
+  var resetBtn = document.getElementById("next");
+  resetBtn.disabled = true;
 }
 
-function resetQuiz(){
-  scoreCount=0;
-  currentQuestion=0;
-  quizContainer.style.display='';
-  next.style.display='';
-  score.style.display='none';
-  tryAgain.style.display='none';
+function resetQuiz() {
+  scoreCount = 0;
+  currentQuestion = 0;
+  quizContainer.style.display = "";
+  next.style.display = "";
+  score.style.display = "none";
+  tryAgain.style.display = "none";
   loadQuestion(currentQuestion);
 }
 
